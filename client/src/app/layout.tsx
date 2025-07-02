5b62d86336d63f19968f2f8ce5feb826d6c7fd72
ashim <magarashim69086@gmail.com>
2025-07-02 14:36:38 +0545

    auth image

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black w-screen h-screen`}
       >
-
+        <SpeedInsights />
+        
         <Provider>
 
           {children}
